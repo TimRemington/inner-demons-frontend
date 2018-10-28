750327e6560cc594f5f934b147080904812549ee
Tim Watervoort <twwatervoort@gmail.com>
2018-10-28 09:31:29 -0600

    added html

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 document.addEventListener(`DOMContentLoaded`, () => {
 
 });
+
+const goalsDropdown = document.querySelector('#goalsDropdown');
+const weaponsDropdown = document.querySelector('#weaponsDropdown');
+const monstersDropdown = document.querySelector('#monstersDropdown');
+
+function makeCard(drop) {
+  drop.appendChild(makeDiv('card'));
+}
+
+function makeDiv(cl) {
+  let div = document.createElement('div');
+  cl.forEach(x => {
+    div.classList.add(x);
+  });
+  return div;
+}
