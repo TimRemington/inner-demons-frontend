1041d482ecc0d52d087bfecf09869be6f93d6a70
Tim Watervoort <twwatervoort@gmail.com>
2018-10-29 15:57:44 -0600

    small fix

--- scripts/intro.js
+++ scripts/intro.js
@@ -11,13 +11,17 @@ document.addEventListener('DOMContentLoaded', () => {
   fullForm.addEventListener('submit', event => {
     event.preventDefault();
     axios.get(`http://localhost:3000/users`)
-    .then(result => {
-      let users = result.data.filter(x => {
-        return x.name === form.value
+      .then(result => {
+        let users = result.data.filter(x => {
+          return x.name === form.value
+        });
+        if (users.length > 0) {
+          localStorage.setItem('user', users[0].id)
+          location.replace('../index.html')
+        } else {
+          form.value = 'user does not exist';
+        }
       });
-      localStorage.setItem('user', users[0].id)
-      location.replace('../index.html')
-    });
   });
 
 });
